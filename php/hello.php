016ff9dc45f985a07ded48c77695c56df7ae3866
centos7 <centos7@localhost>
2017-12-21 02:55:56 +0800

    add form.php demo

--- php/hello.php
+++ php/hello.php
@@ -2,7 +2,7 @@
 
 require_once 'servlet.php';
 
-class embed implements \hi\servlet {
+class hello implements \hi\servlet {
 
     public function handler(\hi\request &$req, \hi\response &$res) {
         $res->content = 'hello,world';
