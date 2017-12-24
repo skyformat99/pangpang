b6e4b370337152d69309a5dd07c7f2c4c75b928b
centos7 <centos7@localhost>
2017-12-25 07:42:16 +0800

    update form demo

--- php/form.php
+++ php/form.php
@@ -5,9 +5,21 @@ require_once 'hi/servlet.php';
 
 class  form implements \hi\servlet{
     public function handler(\hi\request &$req,\hi\response &$res){
-        $res->content = "";
+        $res->content = "form :<br />";
         foreach ($req->form as $key => $value) {
-            $res->content .= $key ."=".$value."<br />";
+            $res->content .= $key . "=" . $value . "<br />";
+        }
+        $res->content .= "headers :<br />";
+        foreach ($req->headers as $key => $value) {
+            $res->content .= $key . "=" . $value . "<br />";
+        }
+        $res->content .= "cookies :<br />";
+        foreach ($req->cookies as $key => $value) {
+            $res->content .= $key . "=" . $value . "<br />";
+        }
+        $res->content .= "session :<br />";
+        foreach ($req->session as $key => $value) {
+            $res->content .= $key . "=" . $value . "<br />";
         }
         $res->status =200;
     }
